1bda6ef097c09cd7e1a3a2837898132cb74c252e
anchernyh <anchernykh15@gmail.com>
2022-01-28 17:02:59 +0200

    пощади

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,5 +4,10 @@ public class MyFirstProgram
 {
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
+        double a=4;
+        System.out.println("Площадь квадрата со стороной "+a+" = "+area(a));
 	}
+    public static double area(double a){
+        return a*a;
+    }
 }
