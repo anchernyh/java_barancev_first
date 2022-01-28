896fe39539ce4d6b8dc7aadc3525c4363d05d482
anchernyh <anchernykh15@gmail.com>
2022-01-28 17:41:03 +0200

    площадь квадрата и прямоугольника

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,10 +4,9 @@ public class MyFirstProgram
 {
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
-        double a=4;
-        System.out.println("Площадь квадрата со стороной "+a+" = "+area(a));
+        Square s = new Square(5) ;
+        System.out.println("Площадь квадрата со стороной "+s.l+" = "+s.area());
+		Rectangle r = new Rectangle(4,6);
+		System.out.println("Площадь прямоугольника со сторонами "+r.a+" и "+r.b+" = "+r.area());
 	}
-    public static double area(double a){
-        return a*a;
-    }
 }
